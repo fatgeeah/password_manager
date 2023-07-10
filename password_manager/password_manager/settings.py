633e0ed2b9c7fba59880273887aec888d7266a62
fatgeeah <fatgeeahminkis@gmail.com>
2023-07-10 13:58:26 +0200

    code

--- password_manager/password_manager/settings.py
+++ password_manager/password_manager/settings.py
@@ -119,9 +119,20 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_HOST_USER = "Pmanager165@gmail.com"
+EMAIL_HOST_PASSWORD = "spnfoxicmpwvkuex"
+EMAIL_USE_TLS = True
